31b59c1824ca2f056d056f0a8cf3f5d1e9d26c3f
leabernard <lbernard@la-providence.net>
2021-01-26 16:08:43 +0100

    comit

---
+++ exo1.js
@@ -0,0 +1,10 @@
+onclick = function(){
+    
+    var longeur = prompt("longeur");
+    var largeur = prompt("largeur");
+    longeur = parseInt(longeur, 10);
+    largeur = parseInt(largeur, 10);
+    const aire = longeur * largeur;
+    const per = (longeur + largeur) * 2;
+    alert("l'aire est de" +aire + " le périmetre est de " + per);
+};
---
+++ exo1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+<input type="button" value="bouton">
+
+
+
+
+
+
+
+
+
+<script type="text/javascript" src="exo1.js"></script>
+    
+</body>
+</html>
