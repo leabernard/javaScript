3294f29d0de7dfb0191b3812f9abb6ac3f234921
leabernard <lbernard@la-providence.net>
2021-01-26 17:34:54 +0100

    exo4

---
+++ exo4.js
@@ -0,0 +1,12 @@
+var tab=[-2,1,4];
+
+function addictionne(x){
+return x+2;
+
+}
+function affiche(){
+
+    alert(addictionne(-2));
+    alert(addictionne(1));
+    alert(addictionne(4));
+}
---
+++ exo4.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exo4</title>
+
+</head>
+<body>
+<button onclick="affiche();">bouton</button>
+
+
+
+
+<script type="text/javascript"  src="exo4.js"></script>
+</body>
+</html>
