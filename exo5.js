68b7eb085e055f5d58d9b1964c119b85e1af31e4
leabernard <lbernard@la-providence.net>
2021-01-28 11:12:55 +0100

    exo5

---
+++ exo5.js
@@ -0,0 +1,30 @@
+function addictionne()
+    {
+        // for(i=0;i<array[longueur];i++){
+        //     var tab =[-2,1,4];
+        // }
+
+    }
+
+
+
+
+function boucle() {
+
+    let tab = new Array[3];
+
+    for (i = 0; i < tab.length; i++)
+        tab[i] = i * i;
+}
+
+function boucle2(){
+    
+    const longueur = parseInt(prompt("entrez la longueur"));
+    let tab = [];
+
+    for(i=0;i< longueur; i++)
+        tab[i] = i * i;
+
+    alert(tab);
+
+}
---
+++ exo5.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exo5</title>
+
+</head>
+<body>
+<button onclick="boucle();">bouton</button>
+<button onclick="boucle2();">bouton</button>
+
+
+
+<script type="text/javascript"  src="exo5.js"></script>
+</body>
+</html>
