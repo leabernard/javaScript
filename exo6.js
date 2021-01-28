e4ba550afe8674ae7687630bf851a956dcb3c443
leabernard <lbernard@la-providence.net>
2021-01-28 11:57:07 +0100

    exo6

---
+++ exo6.js
@@ -0,0 +1,22 @@
+var tab=[-2,1,4];
+var prem = soustrait(tab[0]);
+var der = soustrait(tab[tab.length-1]);
+function soustrait(x){
+   
+   if(x >= 0){
+
+   return resultat = x-2;
+
+   }else{
+      return resultat =" Nombre négatif!"
+
+   }
+
+}
+function affiche(){
+
+    
+
+    alert(prem);
+    alert(der);
+}
