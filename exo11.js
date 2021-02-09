11bfb1c8e1c9c419f0c6830e461328704b633858
leabernard <lbernard@la-providence.net>
2021-02-09 17:30:32 +0100

    exo11

---
+++ exo11.js
@@ -0,0 +1,12 @@
+function centrage_h1(){
+    const h1 = document.getElementById("h1");
+    h1.innerHTML = "STI3!";
+    h1.style.margin = "0px auto" ;
+    h1.style.width = "0px ";
+}
+function centrage_h2(){
+    const h1 = document.getElementById("h1");
+    h1.style.margin = "0 %" ;
+    h1.style.width = "100%";
+    h1.innerHTML = "les BTS SN1 cest les meilleurs";
+}
---
+++ exo11.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exo11</title>
+
+</head>
+<body>
+<div> 
+    <h1 width="500px" onclick="centrage_h1()" ondblclick="centrage_h2()" id="h1">les BTS SN1 cest les meilleurs</h1>
+</div>
+
+
+
+
+<script type="text/javascript"  src="exo11.js"></script>
+</body>
+</html>
