b9eaa1ca5770910cf79727dec7839296ea869840
leabernard <lbernard@la-providence.net>
2021-02-09 14:31:43 +0100

    exo8

---
+++ exo8.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exo8</title>
+
+</head>
+<body>
+<div onmouseover="this.style.color='red'"; onclick="this.style.color='lime'"; ondblclick="this.style.color='navy'"  > salut je suis donald      </div>
+
+
+
+
+<script type="text/javascript"  src="exo8.js"></script>
+</body>
+</html>
