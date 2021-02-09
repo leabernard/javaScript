5bc433eb62318a13cd99efa17259ced2ce649e37
leabernard <lbernard@la-providence.net>
2021-02-09 15:05:26 +0100

    exo9

---
+++ exo9.js
@@ -0,0 +1,13 @@
+function text(){
+const text = document.getElementById("text");
+    text.style.margin = "0px auto" ;
+    text.style.width = "0px";
+    
+}
+
+function text2(){
+    const text = document.getElementById("text");
+    text.style.margin = "0px auto" ;
+    text.style.width = "100%";
+
+}
---
+++ exo9.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>exo9</title>
+
+</head>
+<body>
+<div width="500px" onclick="text();" id="text" ondblclick="text2();"  > la  réserve opérationnelle est composée de volontaires qui s'engagent à servir dans les armées (Terre, Marine nationale...). Pour être candidat à la réserve opérationnelle, vous devez respecter certaines conditions (âge, aptitude physique, nationalité,...). Vous signez un contrat d'engagement pour une durée d'1 à 5 ans, renouvelable une fois. Vous recevez une rémunération et certains autres avantages. Vous pouvez rompre ou suspendre votre engagement     </div>
+
+
+
+
+<script type="text/javascript"  src="exo9.js"></script>
+</body>
+</html>
