b334836f8efa703f0ffda3a6e34e8bfe2744b03a
leabernard <lbernard@la-providence.net>
2021-02-18 10:39:13 +0100

    modif

--- exo11.js
+++ exo11.js
@@ -1,6 +1,6 @@
 function centrage_h1(){
     const h1 = document.getElementById("h1");
-    h1.innerHTML = "STI3!";
+    h1.innerHTML = "Nan les STI3!";
     h1.style.margin = "0px auto" ;
     h1.style.width = "0px ";
 }
